3245e78832dd76117e7548a37c714593e02f0a39
Javier de Arcos <javier.dearcos@liferay.com>
2021-01-12 16:24:30 +0100

    I think it is better to use DELETE instead of UPDATE in the addAction method in PriceListAccountResourceImpl

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/PriceListAccountResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/PriceListAccountResourceImpl.java
@@ -167,7 +167,7 @@ public class PriceListAccountResourceImpl
 					commercePriceListAccountRel.getCommercePriceList();
 
 				return addAction(
-					"UPDATE",
+					"DELETE",
 					commercePriceListAccountRel
 						.getCommercePriceListAccountRelId(),
 					"deletePriceListAccount",
