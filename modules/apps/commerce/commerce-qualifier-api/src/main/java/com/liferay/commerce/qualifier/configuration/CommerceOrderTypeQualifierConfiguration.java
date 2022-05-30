c6445266e4b22e2aa109b9a8da47bd5c8baf97d5
Riccardo Alberti <riccardo.alberti@liferay.com>
2022-05-31 00:48:50 +0200

    COMMERCE-9279 Add configuration classes

---
+++ modules/apps/commerce/commerce-qualifier-api/src/main/java/com/liferay/commerce/qualifier/configuration/CommerceOrderTypeQualifierConfiguration.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.qualifier.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Riccardo Alberti
+ */
+@ExtendedObjectClassDefinition(
+	category = "qualifiers", scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.commerce.qualifier.configuration.CommerceOrderTypeQualifierConfiguration",
+	localization = "content/Language",
+	name = "commerce-order-type-qualifier-configuration-name"
+)
+public interface CommerceOrderTypeQualifierConfiguration
+	extends CommerceQualifierConfiguration {
+
+	@Meta.AD(
+		deflt = "channel", name = "allowed-target-key-groups", required = false
+	)
+	public String[] allowedTargetKeyGroupsArray();
+
+	@Meta.AD(
+		deflt = "channel", name = "order-by-target-key-groups", required = false
+	)
+	public String[] orderByTargetKeyGroupsArray();
+
+}
---
+++ modules/apps/commerce/commerce-qualifier-api/src/main/java/com/liferay/commerce/qualifier/configuration/CommercePaymentMethodGroupRelQualifierConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.qualifier.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Riccardo Alberti
+ */
+@ExtendedObjectClassDefinition(
+	category = "qualifiers", scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.commerce.qualifier.configuration.CommercePaymentMethodGroupRelQualifierConfiguration",
+	localization = "content/Language",
+	name = "commerce-payment-method-group-rel-qualifier-configuration-name"
+)
+public interface CommercePaymentMethodGroupRelQualifierConfiguration
+	extends CommerceQualifierConfiguration {
+
+	@Meta.AD(
+		deflt = "order-type", name = "allowed-target-key-groups",
+		required = false
+	)
+	public String[] allowedTargetKeyGroupsArray();
+
+	@Meta.AD(
+		deflt = "order-type", name = "order-by-target-key-groups",
+		required = false
+	)
+	public String[] orderByTargetKeyGroupsArray();
+
+}
