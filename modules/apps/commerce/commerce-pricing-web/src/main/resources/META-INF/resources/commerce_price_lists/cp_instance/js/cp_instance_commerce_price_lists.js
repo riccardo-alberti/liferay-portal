fd5e9b673e59a9bb7e7671ca342c40b96f62608b
Riccardo Alberti <riccardo.alberti@liferay.com>
2023-12-19 12:23:55 +0100

    COMMERCE-12963 Remove feature flags from js

--- modules/apps/commerce/commerce-pricing-web/src/main/resources/META-INF/resources/commerce_price_lists/cp_instance/js/cp_instance_commerce_price_lists.js
+++ modules/apps/commerce/commerce-pricing-web/src/main/resources/META-INF/resources/commerce_price_lists/cp_instance/js/cp_instance_commerce_price_lists.js
@@ -3,106 +3,46 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-function addOpenSelectionModalEvent({
-	eventNames,
-	keys,
-	namespace,
-	titles,
-	url,
-	urls,
-}) {
-	if (Liferay.FeatureFlags['COMMERCE-11287']) {
-		Liferay.on(namespace + 'addCommercePriceEntry', () => {
-			openSelectionModal({
-				namespace,
-				url,
-			});
+function addOpenSelectionModalEvent({namespace, url}) {
+	Liferay.on(namespace + 'addCommercePriceEntry', () => {
+		openSelectionModal({
+			namespace,
+			url,
 		});
+	});
 
-		Liferay.on('destroyPortlet', () => {
-			Liferay.detach(namespace + 'addCommercePriceEntry');
-		});
-	}
-	else {
-		for (let i = 0; i < keys.length; i++) {
-			Liferay.on(namespace + eventNames[i], () => {
-				openSelectionModal({
-					key: keys[i],
-					namespace,
-					title: titles[i],
-					url: urls[i],
-				});
-			});
-		}
-		Liferay.on('destroyPortlet', () => {
-			for (let i = 0; i < keys.length; i++) {
-				Liferay.detach(namespace + eventNames[i]);
-			}
-		});
-	}
+	Liferay.on('destroyPortlet', () => {
+		Liferay.detach(namespace + 'addCommercePriceEntry');
+	});
 }
 
-function openSelectionModal({key, namespace, title, url}) {
+function openSelectionModal({namespace, url}) {
 	const openerWindow = Liferay.Util.getOpener();
 
-	if (Liferay.FeatureFlags['COMMERCE-11287']) {
-		openerWindow.originalOpenerLiferay = Liferay;
+	openerWindow.originalOpenerLiferay = Liferay;
 
-		return openerWindow.Liferay.Util.openModal({
-			buttons: [
-				{
-					displayType: 'secondary',
-					label: Liferay.Language.get('cancel'),
-					type: 'cancel',
-				},
-				{
-					formId: `${namespace}addPriceEntryForm`,
-					label: Liferay.Language.get('add'),
-					type: 'submit',
-				},
-			],
-			containerProps: {
-				className: 'add-price-entry-modal modal-height-lg',
+	return openerWindow.Liferay.Util.openModal({
+		buttons: [
+			{
+				displayType: 'secondary',
+				label: Liferay.Language.get('cancel'),
+				type: 'cancel',
 			},
-			id: `${namespace}addPriceEntryDialog`,
-			iframeBodyCssClass: '',
-			size: 'lg',
-			title: Liferay.Language.get('add-price'),
-			url,
-		});
-	}
-	else {
-		const addCommercePriceEntryFm = document.getElementById(
-			namespace + 'addCommercePriceEntryFm'
-		);
-		const commercePriceListIds = document.getElementById(
-			namespace + 'commercePriceListIds'
-		);
-		const unitOfMeasureKeyInput = document.getElementById(
-			namespace + 'unitOfMeasureKey'
-		);
-
-		openerWindow.Liferay.Util.openSelectionModal({
-			multiple: true,
-			onSelect: (selectedItems) => {
-				if (!selectedItems || !selectedItems.length) {
-					return;
-				}
-				if (commercePriceListIds) {
-					commercePriceListIds.value = selectedItems;
-				}
-				if (key && unitOfMeasureKeyInput) {
-					unitOfMeasureKeyInput.value = key;
-				}
-				if (addCommercePriceEntryFm) {
-					submitForm(addCommercePriceEntryFm);
-				}
+			{
+				formId: `${namespace}addPriceEntryForm`,
+				label: Liferay.Language.get('add'),
+				type: 'submit',
 			},
-			selectEventName: 'priceListsSelectItem',
-			title,
-			url,
-		});
-	}
+		],
+		containerProps: {
+			className: 'add-price-entry-modal modal-height-lg',
+		},
+		id: `${namespace}addPriceEntryDialog`,
+		iframeBodyCssClass: '',
+		size: 'lg',
+		title: Liferay.Language.get('add-price'),
+		url,
+	});
 }
 
 export default function (context) {
